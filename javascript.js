7c15f1f6b1a4a0205537b014b7b8574ae9eaba38
Aleksandr Kyrylchuk <al.kyrylchuk@gmail.com>
2022-01-24 16:56:05 +0100

    first commit

---
+++ javascript.js
@@ -0,0 +1,11 @@
+let name = prompt(`как тебя зовут?`);
+alert(`привет ${name}`);
+
+let age = prompt('в каком году ты родился/лась?');
+const YEAR = '2022';
+alert(`возраст ${YEAR - age}`);
+
+let sideLengthSquare = prompt(`назови длину стороны квадрата`);
+alert(`периметр равен ${sideLengthSquare * 4}`);
+
+
---
+++ max/javascript.js
@@ -0,0 +1,12 @@
+let circleRadius = prompt(`назови радиус окружности`);
+const  THE_NUMBER_PI = `3.14`
+alert(`площадь круга равна ${THE_NUMBER_PI * (circleRadius * circleRadius)}`);
+
+let distance = prompt(`назви расстояние между городами (км)`);
+let timeToTrevel = prompt(`назови время за которое необходимо добраться до места назначения (ч)`);
+alert(`необхоим двигаться о скоростью ${distance / timeToTrevel} км/ч`);
+
+const DOLLAR = `28.2`;
+const EURO = `31.85`;
+let dollarSum = prompt(`введите сумму в долларах`);
+alert(`сумма в евро ${DOLLAR * dollarSum / EURO}`);
